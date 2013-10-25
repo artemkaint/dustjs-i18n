ff0513d77628fad6294a6eac8640e6517e858c96
Manuel Martín <manuel.martin@gmail.com>
2013-10-25 17:58:25 +0200

    Added:  - Now you can add keys to a language  - helper i18n is defined and ready for a very basic use

--- test/dust-i18n_test.js
+++ test/dust-i18n_test.js
@@ -1,48 +1,78 @@
-'use strict';
-var dust;
+(function(){
+  'use strict';
 
-exports['dust i18n helper'] = {
+  var dust = require('dustjs-linkedin'),
+      compile = function(template, name){
+        var compiled = dust.compile(template, name);
+        dust.loadSource(compiled);
+      };
+  
+  require('../lib/dust-i18n.js');
 
-  setUp: function(done) {
-    dust = require('dustjs-linkedin');
-    require('../lib/dust-i18n.js');
+  exports['dust i18n helper'] = {
 
-    done();
-  },
+    setUp: function(done) {
+      dust.i18n.resetContext();
 
-  'it should throw exception if selected language is not setted': function(test) {
-    test.expect(1);
+      // clears DustJS cache
+      Object.keys(dust.cache).forEach(function(key){
+        delete dust.cache[key];
+      });
+
+      done();
+    },
+
+    'it should throw exception if selected language is not setted': function(test) {
+      test.expect(1);
     
-    test.throws(function(){
-      dust.i18n.setLanguage('es_ES');
-    }, "language 'es_ES' not available!");
+      test.throws(function(){
+        dust.i18n.setLanguage('es_ES');
+      }, /language 'es_ES' not available/);
 
-    test.done();
-  },
-
-  'it should not throw exception if selected language is setted': function(test) {
-    test.expect(1);
-
-    dust.i18n.setLanguages(['es_ES']);
-    
-    test.doesNotThrow(function() {
-      dust.i18n.setLanguage(['es_ES']);
-    });
-
-    test.done();
-  }  
-
-  /*
-  'it should throw exception if language is not setted': function(test) {
-    test.expect(1);
-    // tests here
-    var compiled = dust.compile('{@i18n key="hello_world"/}', 'hello_world');
-    dust.loadSource(compiled);
-
-    dust.render('hello_world', {}, function(err, out) {
-      test.equal(out, 'Hello world!', 'should be Hello world!');
       test.done();
-    });
-  }
-  */
-};
+    },
+
+    'it shouldn\'t throw exception if selected language is setted': function(test) {
+      test.expect(1);
+
+      dust.i18n.setLanguages(['es_ES']);
+    
+      test.doesNotThrow(function() {
+        dust.i18n.setLanguage(['es_ES']);
+      });
+
+      test.done();
+    },
+
+    'it should add keys to an available language': function(test) {
+      test.expect(1);
+
+      dust.i18n.setLanguages(['es_ES']);
+      dust.i18n.add('es_ES', {
+        'test.hello_world' : '¡Hola mundo!'
+      });
+
+      compile('{@i18n key="test.hello_world"/}', 'hello_world');
+      dust.render('hello_world', {}, function(err, out) {
+        test.equal(out, '¡Hola mundo!', 'should be \'¡Hola mundo!\'');
+        test.done();
+      });
+    },
+
+    'it shouldn\'t add keys to an unavailable language': function(test) {
+      test.expect(1);
+
+      dust.i18n.setLanguages(['en_EN']);
+      dust.i18n.add('es_ES', {
+        'test.hello_world' : '¡Hola mundo!'
+      });
+
+      compile('{@i18n key="test.hello_world"/}', 'hello_world');
+      dust.render('hello_world', {}, function(err, out) {
+        test.equal(out, '', 'should be \'\'');
+        test.done();
+      });
+    }
+
+  };
+})();
