cbc634bdae9baa41ea10d32d6d446e8e3b6c5b4f
Manuel Martín <manuel.martin@gmail.com>
2013-10-26 20:26:55 +0200

    Changed key for $key Now strings could be parametrized

--- test/dust-i18n_test.js
+++ test/dust-i18n_test.js
@@ -53,7 +53,7 @@
         'test.hello_world' : '¡Hola mundo!'
       });
 
-      compile('{@i18n key="test.hello_world"/}', 'hello_world');
+      compile('{@i18n $key="test.hello_world"/}', 'hello_world');
       dust.render('hello_world', {}, function(err, out) {
         test.equal(out, '¡Hola mundo!', 'should be \'¡Hola mundo!\'');
         test.done();
@@ -68,7 +68,7 @@
         'test.hello_world' : '¡Hola mundo!'
       });
 
-      compile('{@i18n key="test.hello_world"/}', 'hello_world');
+      compile('{@i18n $key="test.hello_world"/}', 'hello_world');
       dust.render('hello_world', {}, function(err, out) {
         test.equal(out, '', 'should be \'\'');
         test.done();
@@ -83,7 +83,7 @@
         'test.hello_world' : '¡Hola mundo!'
       });
 
-      compile('{@i18n key="{key}"/}', 'hello_world');
+      compile('{@i18n $key="{key}"/}', 'hello_world');
       dust.render('hello_world', {key: "test.hello_world"}, function(err, out) {
         test.equal(out, '¡Hola mundo!', 'should be \'¡Hola mundo!\'');
         test.done();
@@ -102,6 +102,76 @@
       dust.render('hello_world', {}, function(err, out) {
         test.equal(out, '', 'should be \'\'');
         test.done();
+      });
+    },
+
+    'it should resolve one parameter': function(test) {
+      test.expect(1);
+
+      dust.i18n.setLanguages(['es_ES']);
+      dust.i18n.add('es_ES', {
+        'test.hello_world' : '¡Hola {name}!'
+      });
+
+      compile('{@i18n $key="test.hello_world" name="mundo"/}', 'hello_world');      
+      dust.render('hello_world', {}, function(err, out) {
+        test.equal(out, '¡Hola mundo!', 'should be \'¡Hola mundo!\'');
+        test.done();
+      });
+    },
+
+    'it should resolve multiple parameters': function(test) {
+      test.expect(1);
+
+      dust.i18n.setLanguages(['es_ES']);
+      dust.i18n.add('es_ES', {
+        'test.hello_world' : '¡Hola {name} {surname}!'
+      });
+
+      compile('{@i18n $key="test.hello_world" name="Manuel" surname="Martín"/}', 'hello_world');      
+      dust.render('hello_world', {}, function(err, out) {
+        test.equal(out, '¡Hola Manuel Martín!', 'should be \'¡Hola Manuel Martín!\'');
+        test.done();
+      });
+    },
+
+    'it should resolve parameters from context': function(test) {
+      test.expect(1);
+
+      dust.i18n.setLanguages(['es_ES']);
+      dust.i18n.add('es_ES', {
+        'test.hello_world' : '¡Hola {name} {surname}!'
+      });
+
+      compile('{@i18n $key="test.hello_world" name="{name}" surname="{surname}"/}', 'hello_world');      
+      dust.render('hello_world', {name: 'Manuel', surname: 'Martín'}, function(err, out) {
+        test.equal(out, '¡Hola Manuel Martín!', 'should be \'¡Hola Manuel Martín!\'');
+        test.done();
+      });
+    },
+
+    'it should change between setted languages': function(test) {
+      test.expect(2);
+
+      dust.i18n.setLanguages(['es_ES', 'en_EN']);
+      dust.i18n.add('es_ES', {
+        'test.hello_world' : '¡Hola {name} {surname}!'
+      });
+      dust.i18n.add('en_EN', {
+        'test.hello_world' : 'Hello {name} {surname}!'
+      });
+
+      compile('{@i18n $key="test.hello_world" name="{name}" surname="{surname}"/}', 'hello_world');
+
+      dust.render('hello_world', {name: 'Manuel', surname: 'Martín'}, function(err, out) {
+        test.equal(out, '¡Hola Manuel Martín!', 'should be \'¡Hola Manuel Martín!\'');
+        
+        dust.i18n.setLanguage('en_EN');
+
+        dust.render('hello_world', {name: 'Manuel', surname: 'Martín'}, function(err, out) {
+          test.equal(out, 'Hello Manuel Martín!', 'should be \'¡Hola Manuel Martín!\'');
+          test.done();  
+        });
       });
     }
 
